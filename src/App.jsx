ae2454fa24270f12a6dfb81c0621d1a347fb3a8b
Phong Nguyen <thien-phong.nguyen-dxc@hpe.com>
2024-06-03 14:41:24 +0700

    config theme for mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 
 function App() {
   return (
@@ -7,6 +8,12 @@ function App() {
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
+      <Typography variant="body2" color="text.secondary">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+        neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+        quasi quidem quibusdam.
+      </Typography>
     </>
   )
 }
